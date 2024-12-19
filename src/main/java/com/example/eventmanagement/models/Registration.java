c18ff21ff5f105b1b34a99e4560c43e46b79e27a
Iroshan99 <ampiamarakoon@std.appsc.sab.ac.lk>
2024-12-19 18:00:21 +0530

    registration in models

--- src/main/java/com/example/eventmanagement/models/Registration.java
+++ src/main/java/com/example/eventmanagement/models/Registration.java
@@ -1,4 +1,20 @@
 package com.example.eventmanagement.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Registration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User participant;
+
+    @ManyToOne
+    private Event event;
+
+    // Getters and Setters
 }
+
