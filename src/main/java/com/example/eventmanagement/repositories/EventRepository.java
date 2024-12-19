7e9687e800f6274e27c1d0dda4696f00071ec6a2
Iroshan99 <ampiamarakoon@std.appsc.sab.ac.lk>
2024-12-19 18:07:09 +0530

    event repository

---
+++ src/main/java/com/example/eventmanagement/repositories/EventRepository.java
@@ -0,0 +1,5 @@
+package com.example.eventmanagement.repositories;
+
+import com.example.eventmanagement.models.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface EventRepository extends JpaRepository<Event, Long> {}
