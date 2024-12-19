0608803e0aa5e9c625fdfc13b069921d73f50925
Iroshan99 <ampiamarakoon@std.appsc.sab.ac.lk>
2024-12-19 19:10:07 +0530

    registration repository

---
+++ src/main/java/com/example/eventmanagement/repositories/RegistrationRepository.java
@@ -0,0 +1,6 @@
+package com.example.eventmanagement.repositories;
+
+import com.example.eventmanagement.models.Registration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {}
